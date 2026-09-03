36ba72d0c2d64d42ab5033d5707801444dfd8e9c
George Stephanis <daljo628@gmail.com>
2026-09-03 14:17:45 -0400

    Strengthen CSS suppression of native Gutenberg active tab indicator when AI tab is active

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-escape-html', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => 'ea224f8508f0f5f56169');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-editor', 'wp-element', 'wp-escape-html', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => 'e85d19768bf1c93563bd');
